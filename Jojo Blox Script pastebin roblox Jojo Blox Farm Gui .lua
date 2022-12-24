cc5dd4bdbfc3de74c53bf8c0adc758e82f58dced
PapaJoeX <jaouad.kassaoui@gmail.com>
2022-12-24 20:52:30 +0100

    commit zero

---
+++ Jojo Blox Script pastebin roblox Jojo Blox Farm Gui .lua
@@ -0,0 +1,11 @@
+-- find more powerfull verified and New Scripts here : https://script-pastebin.com
+
+loadstring(game:HttpGet(('https://raw.githubusercontent.com/faxng/public-scripts/master/JOJOBLOX_GUI_UPDATED-obfuscated.lua'),true))()
+
+-- find more powerfull verified and New Scripts here : https://scriptpastebin.io
+
+--[[ Script Description:
+
+							FARM ARROW, REBIRTH, MASK, TP TO LOCATION/FARM MOBS
+						
+]]
